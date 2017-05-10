f97bb6372b9d206d13b02bba766f5d6582680feb
Imam <im.stevanno@gmail.com>
2017-05-11 04:10:23 +0700

    add popup for speakers

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,25 @@ function showTopics(){
 }
 
 $(document).ready(function(){
+    $(".numeric").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
 	$('.owl-carousel').owlCarousel({
         // loop:true,
         margin:10,
@@ -33,7 +52,13 @@ $(document).ready(function(){
             }
         }
     })
-	$('.datepicker').datepicker();
+    $('.transfer-date').datepicker();
+	$('.datepicker').datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "-50:-8",
+        // maxDate: "+1D"
+    });
 	$('.separate-line').each(function(){
 		var get_width = $(this).data('width');
 		$(this).css({'width':get_width});
